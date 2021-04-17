082efaaad108314ab6977736f75c6180cba9697b
Bouraoui Mejri <jbo@wevioo.com>
2021-04-17 12:57:16 +0100

    file upload

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -27,6 +27,11 @@ class Post
      */
     private $objet;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -55,4 +60,17 @@ class Post
 
         return $this;
     }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,11 +3,13 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class PostType  extends AbstractType{
 
@@ -16,6 +18,30 @@ class PostType  extends AbstractType{
         $builder
             ->add('message', TextareaType::class)
             ->add('objet', TextType::class)
+            ->add('imageFile', FileType::class, [
+                'label' => 'Image (png/jpg file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+                ],
+            ])
             ->add('save', SubmitType::class)
         ;
     }
